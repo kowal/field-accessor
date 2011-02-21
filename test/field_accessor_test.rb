e5ff5d7bb117785293566dcdf4fcaebaa5458cc5
Marek Kowalcze <marek.kowalcze@gmail.com>
2011-02-21 21:03:04 +0100

    License, fix test for jruby and ruby 1.9

--- test/field_accessor_test.rb
+++ test/field_accessor_test.rb
@@ -1,8 +1,6 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
+require 'rubygems'
 require 'test/unit'
 require 'field_accessor'
 
